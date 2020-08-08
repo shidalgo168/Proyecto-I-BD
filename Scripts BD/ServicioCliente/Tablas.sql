15257857a232650e45e539f27487f5b4f744ee8b
solkopper <43771284+solkopper@users.noreply.github.com>
2020-08-08 17:18:27 -0600

    Update AtencionCliente

--- Scripts BD/ServicioCliente/Tablas.sql
+++ Scripts BD/ServicioCliente/Tablas.sql
@@ -1,6 +1,21 @@
 create database ServicioCliente;
 
 
+create table ServicioCliente.CentroAtencionCliente(
+	idCentroAtencionCliente int not null auto_increment,
+    descripcion varchar(100) not null,
+    primary key (idCentroAtencionCliente)
+
+) engine = InnoDB;
+create Table ServicioCliente.EncargadoAtencionCliente
+(
+	idEncargadoAtencionCliente int not null auto_increment,
+    nombreEncargado varchar(100) not null,
+    idCentroAtencionCliente int not null,
+    primary key (idEncargadoAtencionCliente),
+    constraint fk_EncargadoAtencionCliente_CentroAtencionCliente foreign key (idCentroAtencionCliente)
+    references ServicioCliente.CentroAtencionCliente (idCentroAtencionCliente)
+) engine = InnoDB;
 
 create Table ServicioCliente.Ticket
 (
@@ -8,9 +23,11 @@ create Table ServicioCliente.Ticket
     issue varchar(100) not null,
     subjectTicket varchar (30) not null,
     statusTicket varchar (10) not null,
+    idEncargadoAtencionCliente int not null,
     idCliente int not null,
-    primary key (idTicket)
-
+    primary key (idTicket),
+    constraint fk_Ticket_EncargadoAtencionCliente foreign key (idEncargadoAtencionCliente)
+    references ServicioCliente.EncargadoAtencionCliente (idEncargadoAtencionCliente)
 )engine = InnoDB; 
 
 create table ServicioCliente.Cupon
