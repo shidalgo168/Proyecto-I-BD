a5a86c69caa83d8271ac35a598601c30bd1d3dad
solkopper <43771284+solkopper@users.noreply.github.com>
2020-08-09 14:40:14 -0600

    Sp verCuponesDisponibles

---
+++ Scripts BD/SPs individuales/verCuponesDisponibles.sql
@@ -0,0 +1,26 @@
+USE [proyectoBases]
+GO
+
+/****** Object:  StoredProcedure [dbo].[verCuponesDisponibles]    Script Date: 09/08/2020 02:38:05 p.m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE procedure [dbo].[verCuponesDisponibles] (@idC int)
+as
+begin 
+	if exists(select idCliente from Cliente where @idC = idCliente)
+		begin
+			
+			select idCupon, porcentaje, fechaExpiracion
+			from openquery (MYSQL, 'select * from serviciocliente.Cupon') where idCliente = @idC and estado = 1
+		end
+	else
+		print('Cliente no existe')
+
+end
+GO
+
+
